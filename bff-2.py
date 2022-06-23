8aa9e5ae4f1580806497e56879a7ca8ad569503d
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2022-06-23 19:27:35 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -1,1 +1,19 @@
-import sys, osM = ('\x1b[1;91m')O = ('\x1b[1;96m')if sys.version_info.major != 3:  exit("\n%s!%s gunakan versi python3 "%(M,O))if __name__=='__main__':    try:        __import__("cracks").Menu()    except Exception as e:        exit(str(e))
+import sys, os, subprocess
+
+bff_2 = open(os.devnull, "w")
+my_music = subprocess.call(["dpkg","-s","play-audio"],stdout=bff_2,stderr=subprocess.STDOUT)
+bff_2.close()
+if my_music !=0:
+	os.system('pkg install play-audio')
+
+M = ('\x1b[1;91m')
+O = ('\x1b[1;96m')
+
+if sys.version_info.major != 3:
+  exit("\n%s!%s gunakan versi python3 "%(M,O))
+
+if __name__=='__main__':
+    try:
+        __import__("cracks").Menu()
+    except Exception as e:
+        exit(str(e))
