35afb320a1cc9eae2257890d4a7693d205d6a2b6
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2022-07-30 01:51:00 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -6,46 +6,36 @@
 # *version 1.3
 
 import os, sys, shutil, subprocess
+	
+#--- CEK VERSI PYTHON
+if sys.version_info.major != 3:
+	exit("\n\t\x1b[0m type: python bff-2.py\n")
 
 #--- INSTALL MODULE
 try:
 	import requests, bs4, mechanize, concurrent.futures, rich
-except (ImportError,ModuleNotFoundError) as ngontol:
+except ImportError as ngontol:
 	print ('\n\t\x1b[0m $ install module ...\n')
-	os.system(
-		'rm -rf data/indo.txt && rm -rf data/english.txt && rm -rf data/pengguna.txt' 
-	)
-	os.system(
-		f'python -m pip install --upgrade pip && pip install {ngontol}' 
-	)
+	os.system("python -m pip install --upgrade pip && pip install "+ngontol)
+	os.system("rm -rf data/indo.txt && rm -rf data/english.txt && rm -rf data/pengguna.txt")
 
 try: # music mp3
 	bff_2 = open(os.devnull, "w")
 	my_music = subprocess.call(["dpkg","-s","play-audio"],stdout=bff_2,stderr=subprocess.STDOUT)
 	bff_2.close()
 	if my_music !=0:
-		os.system(
-			'pkg install play-audio' 
-		)
+		os.system('pkg install play-audio' )
 except FileNotFoundError:
-	os.system(
-		'pkg install play-audio' 
-	)
-			
-#--- CEK VERSI PYTHON
-if sys.version_info.major != 3:
-	exit(
-		"\n\t\x1b[0m type: python bff-2.py\n"
-	)
-	
+	os.system('pkg install play-audio' )
+
 #--- FILE DIRECTORY
 from romz import romz_xyz_ganteng_banget
 	
 runtah = [
-					"romz/__pycache__",
-					"log/__pycache__",
-					"xyz/__pycache__"
-			]
+			"romz/__pycache__",
+			"log/__pycache__",
+			"xyz/__pycache__"
+		]
 try:
 	[shutil.rmtree(xz) for xz in runtah]
 except: 
