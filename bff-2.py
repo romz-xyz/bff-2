d883448c867edcf7c2d87655ad37ab3181f63558
Romi Afrizal <81342754+Mark-Zuck@users.noreply.github.com>
2022-11-01 11:03:58 +0700

    Update bff-2.py

--- bff-2.py
+++ bff-2.py
@@ -10,22 +10,50 @@ if __name__=="__main__":
 	import os, sys, shutil, subprocess 
 	if sys.version_info.major != 3: # cek versi python 
 		exit("\n\t\x1b[0m type: python bff-2.py \n")
-		
-	try: # install module
-		import requests, bs4, mechanize, concurrent.futures, rich
-	except (ImportError,ModuleNotFoundError) as ngontol:
-		print ('\n\t\x1b[0m $ install module ...\n')
-		os.system("python -m pip install --upgrade pip && pip install "+str(ngontol))
-		os.system("rm -rf data/indo.txt && rm -rf data/english.txt && rm -rf data/pengguna.txt")
 	
+	# Don't delete the --> coox <-- file, you will get an error
+	coox = ("rm -rf data/indo.txt && rm -rf data/english.txt && rm -rf data/pengguna.txt")
+	try: # install module
+		import requests 
+	except ImportError:
+		print ('\n\t\x1b[0m $ install module requests ...\n')
+		os.system("python -m pip install --upgrade pip && pip install requests")
+		os.system(coox)
+	try:
+		import bs4 
+	except ImportError:
+		print ('\n\t\x1b[0m $ install module bs4 ...\n')
+		os.system("python -m pip install --upgrade pip && pip install bs4")
+		os.system(coox)
+	try:
+		import mechanize 
+	except ImportError:
+		print ('\n\t\x1b[0m $ install module mechanize ...\n')
+		os.system("python -m pip install --upgrade pip && pip install mechanize")
+		os.system(coox)
+	try:
+		import concurrent.futures 
+	except ImportError:
+		print ('\n\t\x1b[0m $ install module futures ...\n')
+		os.system("python -m pip install --upgrade pip && pip install futures")
+		os.system(coox)
+	try:
+		import rich 
+	except ImportError:
+		print ('\n\t\x1b[0m $ install module rich ...\n')
+		os.system("python -m pip install --upgrade pip && pip install rich")
+		os.system(coox)
+		
 	try: # music mp3
 		bff_2 = open(os.devnull, "w")
 		my_music = subprocess.call(["dpkg","-s","play-audio"],stdout=bff_2,stderr=subprocess.STDOUT)
 		bff_2.close()
 		if my_music !=0:
 			os.system('pkg install play-audio' )
+			#os.system(coox)
 	except FileNotFoundError:
 		os.system('pkg install play-audio' )
+		#os.system(coox)
 			
 	from romz import romz_xyz_ganteng_banget as onichan
 	from shutil import rmtree as lolichan
